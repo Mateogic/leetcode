ea344c84f1102c6518d86afc88a20ae644396a06
mateogic <ljc_2318516761@163.com>
2025-05-14 15:17:17 +0800

    update 构造链表+反转链表

--- listnode.java
+++ listnode.java
@@ -10,6 +10,24 @@ public class listnode {
     }
     // 主函数
     public static void main(String[] args) {
-        
+        listnode head = new listnode(0);
+        listnode cur = head;
+        for(int i = 0;i<5;i++){
+            cur.next = new listnode(i+1);
+            cur = cur.next;
+        }
+        // 反转链表
+        listnode tmp = null, pre = null;
+        cur = head;
+        while(cur!=null){
+            tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        while (pre!=null) {
+            System.out.printf("%d, ", pre.val);
+            pre = pre.next;
+        }
     }
 }
